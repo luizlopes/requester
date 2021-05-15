fb5f66ff712ce96573cd3c3b46ed057c11fcd4ff
Luiz Guilherme de Oliveira Lopes <luiz.lopes@vagas.com.br>
2021-05-15 17:15:15 -0300

    Creating a multiple resquests executor

--- README.md
+++ README.md
@@ -1,21 +1,10 @@
 # Requester
 
-**TODO: Add description**
+Execute multiple requests to an endpoint and counts the time wasted waiting the responses.  
 
-## Installation
+This application was developed to be used for performance tests.
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `requester` to your list of dependencies in `mix.exs`:
+## How to use
 
-```elixir
-def deps do
-  [
-    {:requester, "~> 0.1.0"}
-  ]
-end
-```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/requester](https://hexdocs.pm/requester).
+requester --to https://yourserver/endpoint --count 100  
 
--- lib/requester.ex
+++ lib/requester.ex
@@ -3,16 +3,32 @@ defmodule Requester do
   Documentation for `Requester`.
   """
 
-  @doc """
-  Hello world.
+  def request(endpoint, [count: count]) do
+    IO.puts("endpoint #{endpoint} #{count}")
 
-  ## Examples
+    started_at = Time.utc_now
 
-      iex> Requester.hello()
-      :world
+    1..count
+    |> Enum.map(fn _ -> create_request_task(endpoint) end)
+    |> Enum.map(&Task.await/1)
 
-  """
-  def hello do
-    :world
+    finished_at = Time.utc_now
+
+    total_seconds(started_at, finished_at)
+  end
+
+  defp create_request_task(endpoint) do
+    Task.async(fn -> execute_request(endpoint) end)
+  end
+
+  defp execute_request(endpoint) do
+    HTTPoison.get(endpoint)
+  end
+
+  defp total_seconds(started_at, finished_at) do
+    {started_at_secs, _} = Time.to_seconds_after_midnight started_at
+    {finished_at_secs, _} = Time.to_seconds_after_midnight finished_at
+
+    finished_at_secs - started_at_secs
   end
 end
