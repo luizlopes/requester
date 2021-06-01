ef4072b737f7db3ae52b74de21e8502b97360111
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2021-05-31 22:53:21 -0300

    Handling Task.await timeout

--- lib/requester.ex
+++ lib/requester.ex
@@ -3,16 +3,16 @@ defmodule Requester do
   Documentation for `Requester`.
   """
 
-  def request(endpoint, [count: count]) do
+  def request(endpoint, count: count) do
     IO.puts("endpoint #{endpoint} #{count}")
 
-    started_at = Time.utc_now
+    started_at = Time.utc_now()
 
     1..count
     |> Enum.map(fn _ -> create_request_task(endpoint) end)
-    |> Enum.map(&Task.await/1)
+    |> Enum.map(&handle_task/1)
 
-    finished_at = Time.utc_now
+    finished_at = Time.utc_now()
 
     total_seconds(started_at, finished_at)
   end
@@ -22,12 +22,20 @@ defmodule Requester do
   end
 
   defp execute_request(endpoint) do
-    HTTPoison.get(endpoint)
+    HTTPoison.get(endpoint, [], timeout: 3000, recv_timeout: 3000)
+  end
+
+  defp handle_task(task) do
+    try do
+      Task.await(task, 5000)
+    catch
+      :exit, _ -> IO.puts("aiai")
+    end
   end
 
   defp total_seconds(started_at, finished_at) do
-    {started_at_secs, _} = Time.to_seconds_after_midnight started_at
-    {finished_at_secs, _} = Time.to_seconds_after_midnight finished_at
+    {started_at_secs, _} = Time.to_seconds_after_midnight(started_at)
+    {finished_at_secs, _} = Time.to_seconds_after_midnight(finished_at)
 
     finished_at_secs - started_at_secs
   end
